3da14f53755f18d1c11a709891a173c45e1bfe3b
walimike <walimike139@gmail.com>
2026-01-10 11:52:17 +0300

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 3001
+  },
+  pwa: {
+    name: 'Pharmacy App',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    manifestOptions: {
+      background_color: '#ffffff'
+    }
+  }
 })
